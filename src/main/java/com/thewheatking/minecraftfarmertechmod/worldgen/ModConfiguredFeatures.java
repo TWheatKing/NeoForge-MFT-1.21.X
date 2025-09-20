90d85e8716664e787e4036e312e4f74db323682c
TWheatKing <andrew.wheatcroft2@gmail.com>
2025-09-20 23:11:24 +0100

    prep for world gen :)

---
+++ src/main/java/com/thewheatking/minecraftfarmertechmod/worldgen/ModConfiguredFeatures.java
@@ -0,0 +1,27 @@
+package com.thewheatking.minecraftfarmertechmod.worldgen;
+
+import com.thewheatking.minecraftfarmertechmod.MinecraftFarmerTechMod;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.data.worldgen.BootstrapContext;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
+
+public class ModConfiguredFeatures {
+    //CF (how will it look like) -> PF (where do I put it and how many) -> BM (What biome and at what stage?)
+    public static void bootstrap(BootstrapContext<ConfiguredFeature<?, ?>> context) {
+
+
+    }
+
+    public static ResourceKey<ConfiguredFeature<?, ?>> registerKey(String name) {
+        return ResourceKey.create(Registries.CONFIGURED_FEATURE, ResourceLocation.fromNamespaceAndPath(MinecraftFarmerTechMod.MOD_ID, name));
+    }
+
+    private static <FC extends FeatureConfiguration, F extends Feature<FC>> void register(BootstrapContext<ConfiguredFeature<?, ?>> context,
+                                                                                          ResourceKey<ConfiguredFeature<?, ?>> key, F feature, FC configuration) {
+        context.register(key, new ConfiguredFeature<>(feature, configuration));
+    }
+}
